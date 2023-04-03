2728da18ed41c27fb0b7a4d0ffc6eb3ef15b516b
Web, Blockchain, Fintech <wwit.llt@gmail.com>
2023-04-03 18:41:59 +0900

    Initialize

---
+++ README.md
@@ -0,0 +1,1 @@
+Beeswap - Cross Router Protocol
---
+++ contracts/hardhat.config.js
@@ -0,0 +1,36 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.8.1",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.8.2",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+};
